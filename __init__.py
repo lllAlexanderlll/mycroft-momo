e58d54ad7a46f9d46aac4604b233774042c946b2
Alexander Weber <alex.a.weber@gmx.de>
2019-09-26 12:58:00 +0200

    fixed

--- __init__.py
+++ __init__.py
@@ -11,10 +11,6 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
-
-# TODO: Change "Template" to a unique name for your skill
 class MomoSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
@@ -23,24 +19,10 @@ class MomoSkill(MycroftSkill):
         
         # Initialize working variables used within the skill.
         self.iterests = ["card games", "sports", "movies", "reading", "walking", "gardening"]
-
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
+        
     @intent_handler(IntentBuilder("").require("test.intent"))
     def handle_test_intent(self, message):
-        # In this case, respond by simply speaking a canned response.
-        # Mycroft will randomly speak one of the lines from the file
-        #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("test.intent")
+        self.speak_dialog("test")
 
     # @intent_handler(IntentBuilder("").require("Count").require("Dir"))
     # def handle_count_intent(self, message):
@@ -57,8 +39,8 @@ class MomoSkill(MycroftSkill):
     # it.
     #
     def stop(self):
-        self.speak_dialog("stop.intent")
-       return True
+        self.speak_dialog("stop")
+        return True
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
