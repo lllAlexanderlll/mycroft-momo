0b81e9999fdec78730e9640fa24cae7e2c7c420d
lllAlexanderlll <alex.a.weber@gmx.de>
2019-09-27 15:33:53 +0200

    html integrated

--- webserver/app.py
+++ webserver/app.py
@@ -3,6 +3,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# CORS(app, support_credentials=True)
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 @app.route('/message', methods=['GET'])
 def get_message():
 	message = ""
@@ -10,7 +18,7 @@ def get_message():
 		message = f.read()
 	response = flask.Response(message)
 	response.headers['Access-Control-Allow-Origin'] = '*'
-	return response
+	return message
 
 if __name__ == '__main__':
     app.run(debug=True)
