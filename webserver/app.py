23947295460b4a783cdecef8eadc3c8a89b80740
lllAlexanderlll <alex.a.weber@gmx.de>
2019-09-27 15:14:43 +0200

    html integrated

--- webserver/app.py
+++ webserver/app.py
@@ -8,7 +8,9 @@ def get_message():
 	message = ""
 	with open('/opt/mycroft/skills/mycroft-momo/newMessage', 'r') as f:
 		message = f.read()
-	return message
+	response = flask.Response(message)
+	response.headers['Access-Control-Allow-Origin'] = '*'
+	return response
 
 if __name__ == '__main__':
     app.run(debug=True)
