087238ce13f29faa3984d1d30088e27c3376d431
Alexander Weber <alex.a.weber@gmx.de>
2019-09-27 14:39:39 +0200

    pi

--- webserver/app.py
+++ webserver/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/message', methods=['GET'])
 def get_message():
 	message = ""
-	with open('../../newMessage', 'r') as f:
+	with open('/opt/mycroft/skills/mycroft-momo/newMessage', 'r') as f:
 		message = f.read()
 	return message
 
