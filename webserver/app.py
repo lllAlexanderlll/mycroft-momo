13fa70e022108caa2daa860444753a19f3a9c243
lllAlexanderlll <alex.a.weber@gmx.de>
2019-09-27 14:26:11 +0200

    python

--- webserver/app.py
+++ webserver/app.py
@@ -4,8 +4,11 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/message', methods=['GET'])
-def get_tasks():
-    return "test"
+def get_message():
+	message = ""
+	with open('newMessage', 'r') as f:
+		message = f.read()
+    return message
 
 if __name__ == '__main__':
     app.run(debug=True)
