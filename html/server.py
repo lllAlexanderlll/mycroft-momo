026400a159c369f4017ca0c8be7c71506a372e72
lllAlexanderlll <alex.a.weber@gmx.de>
2019-09-28 07:46:56 +0200

    video

---
+++ html/server.py
@@ -0,0 +1,10 @@
+#!flask/bin/python
+from flask import Flask, request
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('/home/alex/Documents/Projects/Python/mycroft-momo/html/index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
